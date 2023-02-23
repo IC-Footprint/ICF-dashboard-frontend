67524997ed483463a4fed037a798c17578310662
filipe-almeidaa <124148251+filipe-almeidaa@users.noreply.github.com>
2023-02-23 14:52:55 +0000

    Change stylelint version and refactor global styles (#39)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AppRoutes from '@/AppRoutes';
 
 import Providers from '@/helpers/Providers';
+import { globalStyles } from '@/theme/global';
 
 function App() {
   return (
     <Providers>
+      {globalStyles}
       <AppRoutes />
     </Providers>
   );
