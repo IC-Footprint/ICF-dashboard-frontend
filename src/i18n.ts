b51c05e9e1391b340b58800c3d260063da6e7efa
Hamish Hutchings <hamish.hutchings@milence.com>
2023-10-23 17:58:36 +0200

    Add mapping for Es

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import messagesEnGB from '@/translations/messages-en-GB.json';
+import messagesEsES from '@/translations/messages-es-ES.json';
 
 i18n.use(initReactI18next).init({
   fallbackLng: 'en_GB',
@@ -9,6 +10,9 @@ i18n.use(initReactI18next).init({
   resources: {
     en_GB: {
       translation: messagesEnGB
+    },
+    es_ES: {
+      translation: messagesEsES
     }
   },
   interpolation: { escapeValue: false } // React already does escaping
