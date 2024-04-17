487cce8c954fab4bb6489154563c4da0f6a52944
filipe-almeidaa <124148251+filipe-almeidaa@users.noreply.github.com>
2024-04-17 15:55:18 +0100

    Fix the length of the globe points (#106)

--- src/utils/globe-utils.ts
+++ src/utils/globe-utils.ts
@@ -4,7 +4,7 @@ export class GlobeUtils {
   static buildGlobeOptions(): GlobeOptionsModel {
     return {
       pointOfViewAltitude: 2,
-      pointSizeScale: 0.0225,
+      pointSizeScale: 0.0185,
       pointRadius: 0.3
     };
   }
